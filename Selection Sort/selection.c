cc8fec6a5d8e3a6ec35362f1ef3a0dda3c6bd5cb
LochanJangid <lochanjangidcoder@gmail.com>
2026-01-19 19:51:12 +0530

    selection here

---
+++ Selection Sort/selection.c
@@ -0,0 +1,75 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void swap(int *, int *);
+    void selection_sort(int, int[]);
+
+    int main()
+    {
+        // input - size of array
+        int n;
+        printf("Enter size of array: ");
+        scanf("%d", &n);
+
+        // Validate size of array
+        if(n <= 0){
+            printf("Invalid length.\n");
+            return 1;
+        }
+
+        // input - n elements of array.
+        int *arr = malloc(n * sizeof(int));
+
+        // check if memory allocation failed.
+        if(arr == NULL){
+            printf("Allocation Failed.\n");
+            return 1;
+        }
+        printf("Enter array: ");
+        for (int i = 0; i < n; i++)
+        {   
+            if(scanf("%d", &arr[i]) != 1){
+                printf("Invalid Input.\n");
+                free(arr);
+                return 1;
+            }
+        }
+
+        selection_sort(n, arr);
+        
+
+        // print sorted array
+        printf("Sorted Array: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+
+        free(arr);
+        return 0;
+    }
+
+    void selection_sort(int n, int arr[n])
+    {
+        for (int i = 0; i < n-1; i++)
+        {
+            int *p = arr + i;
+            int *q = p + 1;
+            while(q < arr + n)
+            {
+                if(*p > *q){
+                    p = q;
+                }
+                q++;
+            }
+            swap(arr + i, p);
+        }
+    }
+
+    void swap(int *p, int *q)
+    {
+        int tmp = *p;
+        *p = *q;
+        *q = tmp;
+    }
